9fe00d117e519b71549acd6b81ba5e3e01d49332
Michael Bowlin <jaidoutani@gmail.com>
2022-03-16 19:01:28 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData (name, email) {
+    let message = "hello world"
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email,
+        })
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(body => newObj(body.id))
+    
+    .catch(function (error) {
+        document.body.innerHTML = error
+        alert("Unauthorized Access");
+        // console.log(error.message)
+    })
+}
+function newObj (newId) {
+    document.getElementsByTagName("body")[0].innerHTML = newId;
+}
